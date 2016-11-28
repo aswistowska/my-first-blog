cebf71c207c7d5475d7395be164a8791c46a7f13
agata <agata@swistowski.org>
2016-11-28 12:49:03 +0100

    My Django Girls app, firts commit

---
+++ python_intro.py
@@ -0,0 +1,8 @@
+def hej(imie):
+    print('Hej ' + imie + '!')
+dziewczyny = ['Agata', 'Monika', 'Kasia', 'Ola', 'Basia']
+for imie in dziewczyny:
+    hej(imie)
+    print('Kolejna dziewczyna')
+for i in range(1, 6):
+    print(i)    
